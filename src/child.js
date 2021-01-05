db2af2d6c39aa6671bc08b619726056b529d1210
gqadirdepar <38527806+gqadirdepar@users.noreply.github.com>
2021-01-05 23:50:37 +0500

    counter updated
    counter

--- src/child.js
+++ src/child.js
@@ -5,7 +5,8 @@ const Child=()=>{
     let countervalue= useContext(Countercontext)
     console.log(countervalue)
     return(
-        <div>This is first child </div>
+        <div>This is first child 
+        <h2>This is counter value {countervalue}</h2> </div>
     )
 }
 export default Child
